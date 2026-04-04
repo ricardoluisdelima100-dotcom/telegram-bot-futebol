9278058f593a93d14ddee36babcc23a6e0266378
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-04 16:39:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+const fetch = require("node-fetch");
+
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+
 console.log("BOT ONLINE 🚀");
 
+async function enviarMensagem() {
+  await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: "🔥 TESTE: BOT FUNCIONANDO!"
+    })
+  });
+}
+
 setInterval(() => {
-  console.log("RODANDO...");
-}, 5000);
+  console.log("Enviando mensagem...");
+  enviarMensagem();
+}, 30000);
