37d51574f29030deb03b6d1007a3f7ad6ffb3152
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-05 13:09:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const TOKEN = "COLE_SEU_TOKEN_AQUI";
+const TOKEN = "Token for the bot Bot Odds Pro @odds_futebol_bot has been revoked. New token is:
+
+8793955195:AAHbEl-PcFo-vlYFDlGRm_GF9SI3q_xHGCI";
 
 async function responder() {
     try {
