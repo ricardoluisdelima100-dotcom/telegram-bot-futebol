f588c6a5362fadd6ae3b226732b5e9c25c48bbc6
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-05 13:41:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const TOKEN = "8793955195:AAEQuj7wJ6BZaoBoKFQLL-m-JqO_LnrNpcg";
+const TOKEN = "8793955195:AAEQuj7wJ6BZaoBoKFQLL-m-JqO_LnrNpcg  ";
 const CHAT_ID = "5167844978";
 
-async function enviar() {
-  try {
-    const res = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        chat_id: CHAT_ID,
-        text: "🔥 AGORA VAI! TESTE OK"
-      })
-    });
-
-    const data = await res.json();
-    console.log(data);
-  } catch (err) {
-    console.log("ERRO:", err);
-  }
+async function enviarMensagem(texto) {
+  await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: texto
+    })
+  });
 }
 
-enviar();
+setInterval(() => {
+  const msg = `⏰ BOT ATIVO
+
+Hora: ${new Date().toLocaleTimeString()}
+
+🔥 Sistema rodando!`;
+
+  enviarMensagem(msg);
+}, 60000); // 1 minuto
