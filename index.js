95800eb8891a5750f2e120dfc885a5d3ee32093c
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-05 12:52:54 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
-  const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const TOKEN = "8793955195:AAHCkewHUV8iwPVxifd9afha-CLla5sdfFE";
+const TOKEN = "SEU_TOKEN";
 
 async function descobrirChatID() {
     try {
-        const resposta = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates`);
+        const resposta = await fetch(`https://api.telegram.org/bot${TOKEN}/getUpdates?offset=-1`);
         const dados = await resposta.json();
 
-        console.log("RESPOSTA:");
+        console.log("RESPOSTA FINAL:");
         console.log(JSON.stringify(dados, null, 2));
     } catch (erro) {
         console.log("ERRO:", erro);
