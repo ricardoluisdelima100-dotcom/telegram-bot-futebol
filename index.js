a0037a4067e3a034c341e927d23c29fb61e9a484
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-05 14:56:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-console.log("TOKEN:", process.env.TELEGRAM_TOKEN);
-console.log("CHAT_ID:", process.env.ID_DO_CHAT);
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const TOKEN = process.env.TELEGRAM_TOKEN;
+const CHAT_ID = process.env.ID_DO_CHAT;
+
+console.log("TOKEN:", TOKEN);
+console.log("CHAT_ID:", CHAT_ID);
+
+async function enviarMensagem() {
+  try {
+    const res = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        chat_id: CHAT_ID,
+        text: "🔥 AGORA VAI FUNCIONAR!"
+      })
+    });
+
+    const data = await res.json();
+    console.log("RESPOSTA:", data);
+  } catch (err) {
+    console.log("ERRO:", err);
+  }
+}
+
+enviarMensagem();
