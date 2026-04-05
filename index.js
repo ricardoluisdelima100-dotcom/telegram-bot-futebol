df78cc1bb7047bdbc5137bfb39a451603120e3cb
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-05 13:23:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 const TOKEN = "Token for the bot Bot Odds Pro @odds_futebol_bot has been revoked. New token is:
 
-8793955195:AAHbEl-PcFo-vlYFDlGRm_GF9SI3q_xHGCI";
+8793955195:AAFr4qfUsciWx72lKEKA7GK1KgRPg4b-Noc
+";
 const CHAT_ID = "5167844978";
 
 fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage?chat_id=${CHAT_ID}&text=🔥 TESTE FUNCIONANDO`)
