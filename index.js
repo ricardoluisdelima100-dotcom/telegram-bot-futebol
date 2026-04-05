cf8d9d6cb00b8b2bc5ca708f9bbc2423113243f6
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-05 15:20:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,27 +3,17 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const TOKEN = process.env.TELEGRAM_TOKEN;
 const CHAT_ID = process.env.ID_DO_CHAT;
 
-console.log("TOKEN:", TOKEN);
-console.log("CHAT_ID:", CHAT_ID);
-
 async function enviarMensagem() {
-  try {
-    const res = await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        chat_id: CHAT_ID,
-        text: "🔥 AGORA VAI FUNCIONAR!"
-      })
-    });
-
-    const data = await res.json();
-    console.log("RESPOSTA:", data);
-  } catch (err) {
-    console.log("ERRO:", err);
-  }
+  await fetch(`https://api.telegram.org/bot${TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id: CHAT_ID,
+      text: "🔥 BOT RODANDO AUTOMÁTICO"
+    })
+  });
 }
 
-enviarMensagem();
+setInterval(() => {
+  enviarMensagem();
+}, 5000;
