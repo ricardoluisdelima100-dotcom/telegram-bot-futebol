cb71680a440dadac7d0e3115712222e338c86341
ricardoluisdelima100-dotcom <ricardoluisdelima100@gmail.com>
2026-04-12 11:40:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-const API_KEY = process.env.RAPIDAPI_KEY;
-
-async function buscarJogos() {
-  const res = await fetch("https://api-football-v1.p.rapidapi.com/v3/fixtures?next=5", {
+async function teste() {
+  const res = await fetch("https://api-football-v1.p.rapidapi.com/v3/fixtures?next=1", {
     method: "GET",
     headers: {
-      "x-rapidapi-key": API_KEY,
+      "x-rapidapi-key": process.env.RAPIDAPI_KEY,
       "x-rapidapi-host": "api-football-v1.p.rapidapi.com"
     }
   });
 
   const data = await res.json();
-
-  console.log(JSON.stringify(data, null, 2));
+  console.log(data);
 }
 
-buscarJogos();
+teste();
